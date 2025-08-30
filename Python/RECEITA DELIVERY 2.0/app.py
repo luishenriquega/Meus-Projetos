15c27d01faf904bb47985993eefa9a6a09dc47b4
LUIZ GOMES <165418777+luishenriquega@users.noreply.github.com>
2025-08-30 14:34:42 -0300

    Adicionando novas pastas

--- Python/RECEITA DELIVERY 2.0/app.py
+++ Python/RECEITA DELIVERY 2.0/app.py
@@ -1,12 +1,17 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_praca.receber_avaliacao('Luiz', 10)
-restaurante_praca.receber_avaliacao('Ste', 8)
-restaurante_praca.receber_avaliacao('LS', 5)
+bebida_suco = Bebida('Suco de Melancia', 5.0,'grande')
+bebida_suco.aplicar_desconto()
+prato_paozinho = Prato('Pâo de Batata',2.00,'O melhor pão da cidade')
+prato_paozinho.aplicar_desconto()
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_paozinho)
 
 def main():
-    Restaurante.listar_restaurantes()
+    restaurante_praca.exibir_cardapio
 
 if __name__ == '__main__':
     main()
